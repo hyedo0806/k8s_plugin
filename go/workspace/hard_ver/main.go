bc5ca438ec6f74ae283d1ff7574eeee566dc45b5
dohye <dohye0806@gmail.com>
2024-06-03 07:53:41 +0000

    flag : cmd line argument

--- go/workspace/hard_ver/main.go
+++ go/workspace/hard_ver/main.go
@@ -18,13 +18,12 @@ import (
 
 func main() {
 
-    //wordPtr := flag.String("iter", "time")
-    iterPtr := flag.Int("iter", 0, "default: 10iterations")
+    iterPtr := flag.Int("iter", 10, "default: 10iterations")
     timePtr := flag.Int("time", 10, "default: 10sec")
     flag.Parse()
-    //fmt.Println("iter : %d", *iterPtr)
-    //fmt.Println("time : %d", *timePtr)
-    
+    //fmt.Printf("iter : %d", *iterPtr)
+    //fmt.Printf("time : %d", *timePtr)
+
     // kubeconfig 파일 경로 설정
     var kubeconfig string
     if home := homedir.HomeDir(); home != "" {
@@ -44,7 +43,8 @@ func main() {
         log.Fatalf("Error creating Kubernetes client: %v", err)
     }
 
-    for {    
+    iter := 1
+    for {
         // 노드 정보 가져오기
         nodes, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
         if err != nil {
@@ -69,8 +69,8 @@ func main() {
             }
 
         }
-        fmt.Printf("Total Node %d\n", cntNode)
-        fmt.Printf("Ready Node %d\n", cntReadyNode)
+	fmt.Printf("	Node : %d/%d\n",cntReadyNode, cntNode)
+        //fmt.Printf("Ready Node %d\n", cntReadyNode)
 
         // 파드 정보 가져오기 (디폴트 네임스페이스)
         pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
@@ -95,12 +95,19 @@ func main() {
                     //fmt.Printf("%d Type: %s, Status: %s\n",i, condition.Type, condition.Status)
             }
         }
-        fmt.Printf("Total Pod %d\n", cntPod)
-        fmt.Printf("Ready Pod %d\n", cntReadyPod)
+	fmt.Printf("	Pod : %d/%d\n", cntReadyPod, cntPod)
+        //fmt.Printf("Ready Pod %d\n", cntReadyPod)
 
         if cntNode != cntReadyNode || cntPod != cntReadyPod {
-            fmt.Println("Retrying ...")
-		    time.Sleep(10)
+
+		if iter == *iterPtr {
+			fmt.Println("Fail")
+			break
+		}
+		iter += 1
+
+		fmt.Println("Retrying ...")
+		time.Sleep(time.Duration(*timePtr) * time.Second)
 
         } else {
             fmt.Println("Pass")
